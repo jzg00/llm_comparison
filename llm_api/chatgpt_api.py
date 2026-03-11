a9b37b4da8bbbc313caee1de87ca5e8ffe7149e0
Jared <jmzg327@gmail.com>
2026-03-10 21:37:14 -0400

    inline comments

--- llm_api/chatgpt_api.py
+++ llm_api/chatgpt_api.py
@@ -16,16 +16,19 @@ def call_chatgpt(prompt: str, model: str = 'gpt-5.3-chat-latest') -> dict:
     completion = client.chat.completions.create(
     model = model,
     messages=[
-    {"role": "developer", "content": "You are a helpful assistant."},
-    {"role": "user", "content": prompt}
+        {"role": "developer", "content": "You are a helpful assistant."},
+        {"role": "user", "content": prompt}
     ]
+    # temperature control not supported for this model
 )
     return completion.choices[0].message.content
 
-# write function to save response
+# write function to save response in data/raw/chatgpt
 
+
+# quick test
 if __name__ == "__main__":
     example_prompt = "Say hello"
-    task_id = "merge_intervals"
+    task_id = "001"
     response = call_chatgpt(example_prompt)
     print(response)
