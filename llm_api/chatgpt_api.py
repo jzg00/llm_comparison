d7a2b491e5934a4820f5d2aaa5a6a5f869755607
Jared <jmzg327@gmail.com>
2026-03-10 21:01:59 -0400

    write initial call api function

--- llm_api/chatgpt_api.py
+++ llm_api/chatgpt_api.py
@@ -1,4 +1,3 @@
-# functions to send prompts/tasks to chatgpt
 import os
 from dotenv import load_dotenv
 from openai import OpenAI
@@ -7,3 +6,26 @@ import json
 load_dotenv()
 
 client = OpenAI() # api_key=os.environ.get("OPENAI_API_KEY") is default parameter
+
+
+
+def call_chatgpt(prompt: str, model: str = 'gpt-5.3-chat-latest') -> dict:
+    """
+    Send task prompts to chatgpt and return raw responses as dict
+    """
+    completion = client.chat.completions.create(
+    model = model,
+    messages=[
+    {"role": "developer", "content": "You are a helpful assistant."},
+    {"role": "user", "content": prompt}
+    ]
+)
+    return completion.choices[0].message.content
+
+# write function to save response
+
+if __name__ == "__main__":
+    example_prompt = "Say hello"
+    task_id = "merge_intervals"
+    response = call_chatgpt(example_prompt)
+    print(response)
