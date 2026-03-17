fbdfb208ef4879d1e9592340c19d5e3a0c3a4ca6
Jared <jmzg327@gmail.com>
2026-03-17 08:53:36 -0400

    initial claude_api.py functions

--- llm_api/claude_api.py
+++ llm_api/claude_api.py
@@ -1,1 +1,14 @@
-# functions to send prompts/tasks to claude
+import os
+from dotenv import load_dotenv
+from anthropic import Anthropic
+import json
+
+load_dotenv()
+
+client = Anthropic() # api_key=os.environ.get("ANTHROPIC_API_KEY") is default
+
+def call_claude():
+    pass
+
+def save_claude():
+    pass
