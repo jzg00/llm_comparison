6775c5c51a4f1b43908e8f6e161504cd8965f519
Jared <jmzg327@gmail.com>
2026-03-21 15:58:11 -0400

    validate compute_loc functionality

--- metrics/code_metrics.py
+++ metrics/code_metrics.py
@@ -1,8 +1,14 @@
-def compute_loc(file_path):
+def compute_loc(file_path) -> dict:
     with open(file_path, "r") as f:
         lines = f.readlines()
     
+    # returning a dictionary here for easier assembly of dataset
     return {
         "loc": len(lines)
     }
 
+# quick test
+# if __name__=="__main__":
+#     file_path = "data/processed/claude/rest_api_endpoint_run1.py"
+#     loc = compute_loc(file_path)
+#     print(loc)
